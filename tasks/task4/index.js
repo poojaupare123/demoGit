be8c289f1379c3cbfe6df81dcbf1b0479ff7f99e
poojaupare123 <poojaupre123@gmail.com>
2026-01-06 15:46:21 +0530

    add questions

--- tasks/task4/index.js
+++ tasks/task4/index.js
@@ -1,3 +1,15 @@
+// Deliverable:
+
+// Using DOM manipulation, do the following (Don’t modify the HTML file):
+
+// Implement the code as shown in the video.
+
+// Use id as a query to select the basket heading and set its color to brown.
+
+// Change the background color of all even fruit items to red and change their text color to white.
+
+// Remove the default list-style (dots) from the fruit list.
+
 
 // query selector 
 let basketHeading = document.querySelector('#basket-heading');
