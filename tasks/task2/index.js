0f2ef83aed03357301d02be0d66d4a2438453e53
poojaupare123 <poojaupre123@gmail.com>
2026-01-06 12:06:31 +0530

    task 2 js file completed

---
+++ tasks/task2/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+let target = document.getElementsByClassName('fruit');
+
+target[2].style.backgroundColor = 'yellow';
+
+for (let i = 0; i < target.length; i++){
+    target[i].style.fontWeight = 'bold';
+}
