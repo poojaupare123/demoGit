05ea896e77160694e9d551e06cc479a592075859
poojaupare123 <poojaupre123@gmail.com>
2026-01-06 11:57:53 +0530

    first-task question1 js file

---
+++ tasks/task1/question_1.js
@@ -0,0 +1,12 @@
+// Write your code below:
+document.getElementById('main-heading').innerHTML = "Fruit World";
+
+document.getElementById('main-heading').style.color = 'orange';
+
+document.getElementById('header').style.backgroundColor = "green";
+
+document.getElementById('header').style.borderBottom = 'solid orange';
+
+document.getElementById('basket-heading').style.color = "green";
+
+document.getElementById('thanks').innerHTML = "<p>Please visit us again</p>";
